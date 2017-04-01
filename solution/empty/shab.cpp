81734963edf2c4ae7e1f490cb9d7fb0658952463
alexey4eg <baltazar@hotbox.ru>
2017-04-01 14:24:58 +0500

    Пустой проект

---
+++ solution/empty/shab.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+void readInput() {
+}
+
+void solve() {
+
+}
+
+void output() {
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	readInput();
+	solve();
+	output();
+	return 0;
+}
